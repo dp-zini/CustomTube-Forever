71acd3470e6ae23231c83f07c393216b7ca7152e
zini <130931805+dp-zini@users.noreply.github.com>
2026-08-15 12:17:03 -0400

    update 1.3.2

--- firefox beta/changelog.js
+++ firefox beta/changelog.js
@@ -5,12 +5,12 @@ function handleChangelog() {
 	let version = browser.runtime.getManifest().version;
 	if (!changelogHeader) return;
 	if (window.location.href.includes('#install')) {
-		changelogHeader.innerText = "CustomTube was successfully installed!";
+		changelogHeader.innerText = "CustomTube-Forever was successfully installed!";
 	}
 	else if (window.location.href.includes('#check')) {
-		changelogHeader.innerText = `CustomTube is currently on version ${version}.`;
+		changelogHeader.innerText = `CustomTube-Forever is currently on version ${version}.`;
 	}	else {
-		changelogVersion.innerText = `CustomTube has updated to version ${version}!`;
+		changelogVersion.innerText = `CustomTube-Forever has updated to version ${version}!`;
 	}
 }
 
